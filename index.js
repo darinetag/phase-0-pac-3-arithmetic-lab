8967f9c4798a0b1b85f39f11192b42697a5f1467
darinetag <tagdarinefatma@gmail.com>
2023-07-23 20:09:33 +0100

    Done With assignment

---
+++ index.js
@@ -0,0 +1,43 @@
+function add(a,b) {
+ return (a+b)
+ }
+  add (2,2)
+
+ function subtract(a,b) {
+    return (a-b)
+    }
+    subtract (2,2)
+
+ function multiply(a,b) {
+    return (a*b)
+        }
+    multiply (2,2)
+
+ function divide(a,b) {
+    return (a/b)
+        }
+     divide (2,2)
+
+     let number = 5;
+
+     number = 10;
+
+
+function increment(n) {
+    return n + 1;
+  }
+  
+  function decrement(n) {
+    return n - 1;
+  }
+  
+function makeInt(n) {
+    const parsedInt = parseInt(n, 10);
+    return isNaN(parsedInt) ? NaN : parsedInt;
+  }
+  
+  function preserveDecimal(n) {
+    const parsedFloat = parseFloat(n);
+    return isNaN(parsedFloat) ? NaN : parsedFloat;
+  }
+  
